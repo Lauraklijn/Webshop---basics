bab6c3776b05351ffad69d64e4d3f92cdc2cf59c
Lauraklijn <lauraklijn12@hotmail.com>
2020-01-30 12:06:07 +0100

    setup api + redux

---
+++ src/store/products/reducer.js
@@ -0,0 +1,13 @@
+const initialState = { products: [], categories: [] };
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "products/FETCHED": {
+      // => Ask yourself: what is action.payload?
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
